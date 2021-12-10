2b3f4b9049a26764a73f60efea2b1498b4ebfc71
Ioannis Christodoulou <JChrist@users.noreply.github.com>
2021-12-10 17:00:12 +0200

    added fix for list tile theme type

--- lib/src/cupertino_settings_item.dart
+++ lib/src/cupertino_settings_item.dart
@@ -76,8 +76,8 @@ class CupertinoSettingsItemState extends State<CupertinoSettingsItem> {
     /// The width of iPad. This is used to make circular borders on iPad and web
     final isLargeScreen = MediaQuery.of(context).size.width >= 768;
 
-    final ThemeData theme = Theme.of(context);
-    final ListTileTheme tileTheme = ListTileTheme.of(context);
+    final theme = Theme.of(context);
+    final tileTheme = ListTileTheme.of(context);
 
     final iconThemeData = IconThemeData(
       color: widget.enabled
@@ -318,7 +318,10 @@ class CupertinoSettingsItemState extends State<CupertinoSettingsItem> {
               ? iosPressedTileColorDark
               : iosTileDarkColor;
 
-  Color? _iconColor(ThemeData theme, ListTileTheme tileTheme) {
+  Color? _iconColor(ThemeData theme, dynamic tileTheme) {
+    if (tileTheme is! ListTileTheme && tileTheme is! ListTileThemeData) {
+      return null;
+    }
     if (tileTheme.selectedColor != null) {
       return tileTheme.selectedColor;
     }
